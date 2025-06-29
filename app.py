53a268de18bfff14d6ac3fba98ef4390409d5d13
Mukund Savaliya <92025415+git-Mukund@users.noreply.github.com>
2025-06-29 14:36:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from googleapiclient.discovery import build
 from textblob import TextBlob
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -52,4 +53,5 @@ def analyze():
     return jsonify(sentiments)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
